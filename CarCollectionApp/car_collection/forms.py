166ad666708bd4095f35654dcbafce32651bedff
bopzen <boppp@abv.bg>
2023-06-23 14:41:30 +0300

    Finished

--- CarCollectionApp/car_collection/forms.py
+++ CarCollectionApp/car_collection/forms.py
@@ -9,9 +9,63 @@ class ProfileForm(forms.ModelForm):
         widgets = {
             'password': forms.PasswordInput()
         }
+        labels = {
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'profile_picture': 'Profile Picture'
+        }
+
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+        widgets = {
+            'password': forms.PasswordInput()
+        }
+        labels = {
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'profile_picture': 'Profile Picture'
+        }
+
+
+class ProfileDeleteForm(ProfileEditForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__set_hidden_fields()
+
+    def save(self, commit=True):
+        if self.instance:
+            Car.objects.all().delete()
+            self.instance.delete()
+        return self.instance
+
+    def __set_hidden_fields(self):
+        for _, field in self.fields.items():
+            field.widget = forms.HiddenInput()
 
 
 class CarForm(forms.ModelForm):
     class Meta:
         model = Car
         fields = '__all__'
+        labels = {
+            'image_url': 'Image URL'
+        }
+
+
+class CarDeleteForm(CarForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__set_read_only_fields()
+
+    def save(self, commit=True):
+        if self.instance:
+            self.instance.delete()
+        return self.instance
+
+    def __set_read_only_fields(self):
+        for _, field in self.fields.items():
+            field.widget.attrs['disabled'] = 'disabled'
+
--- CarCollectionApp/car_collection/views.py
+++ CarCollectionApp/car_collection/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 
-from CarCollectionApp.car_collection.forms import ProfileForm
+from CarCollectionApp.car_collection.forms import ProfileForm, CarForm, CarDeleteForm, ProfileEditForm, \
+    ProfileDeleteForm
 from CarCollectionApp.car_collection.models import Profile, Car
 
 
@@ -42,28 +43,108 @@ def catalogue(request):
 
 
 def create_car(request):
-    return render(request, 'cars/car-create.html')
+    profile = Profile.objects.first()
+    if request.method == 'GET':
+        form = CarForm()
+    else:
+        form = CarForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('catalogue')
+    context = {
+        'profile': profile,
+        'form': form
+    }
+    return render(request, 'cars/car-create.html', context)
 
 
 def car_details(request, pk):
-    return render(request, 'cars/car-details.html')
+    profile = Profile.objects.first()
+    car = Car.objects.filter(pk=pk).get()
+    context = {
+        'profile': profile,
+        'car': car
+    }
+    return render(request, 'cars/car-details.html', context)
 
 
 def edit_car(request, pk):
-    return render(request, 'cars/car-edit.html')
+    profile = Profile.objects.first()
+    car = Car.objects.filter(pk=pk).get()
+    if request.method == 'GET':
+        form = CarForm(instance=car)
+    else:
+        form = CarForm(request.POST, instance=car)
+        if form.is_valid():
+            form.save()
+            return redirect('catalogue')
+    context = {
+        'profile': profile,
+        'car': car,
+        'form': form
+    }
+
+    return render(request, 'cars/car-edit.html', context)
 
 
 def delete_car(request, pk):
-    return render(request, 'cars/car-delete.html')
+    profile = Profile.objects.first()
+    car = Car.objects.filter(pk=pk).get()
+    if request.method == 'GET':
+        form = CarDeleteForm(instance=car)
+    else:
+        form = CarDeleteForm(request.POST, instance=car)
+        if form.is_valid():
+            form.save()
+            return redirect('catalogue')
+    context = {
+        'profile': profile,
+        'car': car,
+        'form': form
+    }
+    return render(request, 'cars/car-delete.html', context)
 
 
 def profile_details(request):
-    return render(request, 'profile/profile-details.html')
+    profile = Profile.objects.first()
+    cars = Car.objects.all()
+    total_price = sum(car.price for car in cars)
+    context = {
+        'profile': profile,
+        'cars': cars,
+        'total_price': total_price
+    }
+
+    return render(request, 'profile/profile-details.html', context)
 
 
 def edit_profile(request):
-    return render(request, 'profile/profile-edit.html')
+    profile = Profile.objects.first()
+    if request.method == 'GET':
+        form = ProfileEditForm(instance=profile)
+    else:
+        form = ProfileEditForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect('profile-details')
+    context = {
+        'profile': profile,
+        'form': form
+    }
+    return render(request, 'profile/profile-edit.html', context)
 
 
 def delete_profile(request):
-    return render(request, 'profile/profile-delete.html')
+    profile = Profile.objects.first()
+    if request.method == 'GET':
+        form = ProfileDeleteForm(instance=profile)
+    else:
+        form = ProfileDeleteForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect('profile-details')
+    context = {
+        'profile': profile,
+        'form': form
+    }
+    return render(request, 'profile/profile-delete.html', context)
